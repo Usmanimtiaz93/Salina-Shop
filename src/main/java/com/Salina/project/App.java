c5326a8ed45a820ab4ba50f9de8f3e130f51cb26
Usmanimtiaz <usmanimtiaz068@gmail.com>
2026-01-19 21:31:40 +0400

    salina shop first commint

---
+++ src/main/java/com/Salina/project/App.java
@@ -0,0 +1,13 @@
+package com.Salina.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
